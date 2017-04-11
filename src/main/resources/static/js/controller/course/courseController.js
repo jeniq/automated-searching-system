5890a2725dbb1a45dca54604f60af44c973e50fe
Yevhen Gr <gr.yevhen@gmail.com>
2017-04-11 18:11:10 +0300

    Connected front-end with back-end

--- src/main/resources/static/js/controller/course/courseController.js
+++ src/main/resources/static/js/controller/course/courseController.js
@@ -3,14 +3,32 @@
         .controller("CourseController", ["$scope", "$http",
             function ($scope, $http) {
 
-                $scope.request;
-                $scope.source = [];
+                $scope.request = {
+                    string: "",
+                    source: ""
+                };
                 $scope.courseList = [];
 
                 $scope.searchCourseBar = function () {
-                    var requestUrl = "/api/course/search?request=" + $scope.request + "&source=" + $scope.source;
+                    var requestUrl = "/api/course/search?request=" + $scope.request.string +
+                        "&source=" + $scope.request.source;
 
-                    return $http.get(requestUrl)
+                    $scope.searchByRequest(requestUrl).then(function (response) {
+                        $scope.courseList = response.data;
+                    }, function errorCallback(response) {
+                    });
+                };
+
+                $scope.showCoursesTable = function () {
+                    if ($scope.courseList.length > 0) {
+                        return true;
+                    }
+                    return false;
+                };
+
+                // http
+                $scope.searchByRequest = function (url) {
+                    return $http.get(url)
                         .then(function (callback) {
                             callback.isError = false;
                             return callback;
@@ -18,13 +36,6 @@
                             callback.isError = true;
                             return callback;
                         });
-                };
-
-                $scope.showCoursesTable = function () {
-                    if ($scope.courseList.length > 0){
-                        return true;
-                    }
-                    return false;
                 };
 
             }]);
